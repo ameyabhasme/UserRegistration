991c6ad9f1778f44d6554f6867e0893677fed480
ameyabhasme <ameyabhasme5@gmail.com>
2021-08-27 14:33:40 +0530

    Modififed: Checking provided email samples

--- src/com/bridgelabz/userregistration/test/TestEmailId.java
+++ src/com/bridgelabz/userregistration/test/TestEmailId.java
@@ -1,11 +1,11 @@
 package com.bridgelabz.userregistration.test;
 
-import java.util.Scanner;
+import java.util.Arrays;
+import java.util.List;
 
 import com.bridgelabz.userregistration.main.UserRegistration;
 
 public class TestEmailId {
-	static final Scanner SC = new Scanner(System.in);
 
 	public static void checkValidTest(UserRegistration userRegistration, String userEmail) {
 		if (userRegistration.emailID(userEmail)) {
@@ -17,19 +17,33 @@ public class TestEmailId {
 
 	public static void main(String[] args) {
 		UserRegistration userRegistration = new UserRegistration();
-		int key = 0;
-		while (key != 2) {
-				System.out.println("Select From Option\n1) Enter Email Id\n2) Exit");
-				key = SC.nextInt();
-				switch (key) {
-				case 1:
-					System.out.println("Enter Email Id");
-					String userEmail = SC.next();
-					checkValidTest(userRegistration, userEmail);
-					break;
-				default:
-					break;
-			} 
+		List<String> emailSamples = Arrays.asList(
+		"abc@yahoo.com",
+		"abc-100@yahoo.com",
+		"abc-100@yahoo.com",
+		"abc111@abc.com",
+		"abc-100@abc.net",
+		"abc.100@abc.com.au",
+		"abc@1.com",
+		"abc@gmail.com.com",
+		"abc+100@gmail.com",
+
+		"abc",
+		"abc@.com.my",
+		"abc123@gmail.a",
+		"abc123@.com",
+		"abc123@.com.com",
+		".abc@abc.com",
+		"abc()*@gmail.com",
+		"abc@%*.com",
+		"abc..2002@gmail.com",
+		"abc.@gmail.com",
+		"abc@abc@gmail.com",
+		"abc@gmail.com.1a",
+		"abc@gmail.com.aa.au");
+
+		for (String userEmail : emailSamples) {
+			checkValidTest(userRegistration, userEmail);
 		}
 	}
 }
